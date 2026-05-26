ad8921e77745d72417186050ed35cbb1f295ba9e
newtyf <axelskam.123@outlook.es>
2026-05-26 18:16:52 -0500

    feat: se agrego el fragment de dashbord trabajado por bellkin

--- app/src/main/java/com/newtyf/cnp_patients_app/presentation/dashboard/DashboardFragment.java
+++ app/src/main/java/com/newtyf/cnp_patients_app/presentation/dashboard/DashboardFragment.java
@@ -1,66 +1,49 @@
 package com.newtyf.cnp_patients_app.presentation.dashboard;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.newtyf.cnp_patients_app.R;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link DashboardFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.newtyf.cnp_patients_app.R;
+import com.newtyf.cnp_patients_app.presentation.patients.create.PatientCreateFragment;
+
 public class DashboardFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public DashboardFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DashboardFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static DashboardFragment newInstance(String param1, String param2) {
-        DashboardFragment fragment = new DashboardFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public static DashboardFragment newInstance() {
+        return new DashboardFragment();
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_dashboard, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.btnAccionNuevoPac).setOnClickListener(v ->
+                requireActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragment_container, PatientCreateFragment.newInstance())
+                        .addToBackStack(null)
+                        .commit());
+
+        view.findViewById(R.id.btnAccionNuevaConsulta).setOnClickListener(v -> {
+            // TODO: navegar a lista de pacientes para seleccionar consulta
+        });
+
+        view.findViewById(R.id.btnAccionCalculadora).setOnClickListener(v -> {
+            // TODO: calculadora de energía
+        });
+
+        view.findViewById(R.id.btnAccionExportar).setOnClickListener(v -> {
+            // TODO: exportar dieta
+        });
     }
 }
