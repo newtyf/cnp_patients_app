27d3b51eb2f3e23a5e2598aba7405c35f6aa93b8
newtyf <axelskam.123@outlook.es>
2026-05-27 12:27:17 -0500

    feat: se agregó el splashScreen

---
+++ app/src/main/java/com/newtyf/cnp_patients_app/presentation/splash/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.newtyf.cnp_patients_app.presentation.splash;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.newtyf.cnp_patients_app.R;
+import com.newtyf.cnp_patients_app.presentation.auth.login.LoginActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        findViewById(R.id.main).postDelayed(() -> {
+            startActivity(new Intent(this, LoginActivity.class));
+            finish();
+        }, 2000);
+    }
+}
